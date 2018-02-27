0c72debd90740e1ebaf6004f522e0dd0275c10a1
Igor.Sharendo <igor@sharendo.ru>
2018-02-27 10:27:24 +0700

    делаем заготовку от Nano title

--- Views/Layout/Layout.php
+++ Views/Layout/Layout.php
@@ -2,7 +2,7 @@
 <html lang="ru">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title>Шаблонизатор</title>
+        <title>Nano MVC</title>
     </head>
     <body>
     <?= $content ?>
